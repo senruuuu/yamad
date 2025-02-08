1ffde8c78416e21d5b82c0fce25b5da4079ee103
Sanroe Estabillo <sanroeestabillo@gmail.com>
2025-02-08 16:15:08 +0800

    edit

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// script.js
-
 // Function to handle button click events
 function selectOption(option) {
-    // Check which option was clicked
     if (option === 'yes') {
         // Flash rainbow colors
-        flashRainbowColors(function() {
+        flashRainbowColors(function () {
             document.getElementById('question').style.display = 'none'; // Hide the question
-            displayCatHeart(); // Display the cat-heart.gif
+            slideInPicture(); // Slide in the custom picture
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
+        document.getElementById('no-button').innerText = 'You sure?';
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
+        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by 2x
         yesButton.style.fontSize = newSize + 'px';
     } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
         alert('Invalid option!');
     }
 }
@@ -27,11 +21,11 @@ function selectOption(option) {
 function flashRainbowColors(callback) {
     var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
     var i = 0;
-    var interval = setInterval(function() {
+    var interval = setInterval(function () {
         document.body.style.backgroundColor = colors[i];
         i = (i + 1) % colors.length;
     }, 200); // Change color every 200 milliseconds
-    setTimeout(function() {
+    setTimeout(function () {
         clearInterval(interval);
         document.body.style.backgroundColor = ''; // Reset background color
         if (callback) {
@@ -40,7 +34,39 @@ function flashRainbowColors(callback) {
     }, 2000); // Flash colors for 2 seconds
 }
 
-// Function to display the cat.gif initially
+// Function to slide in a custom picture
+function slideInPicture() {
+    // Clear existing content in the image container
+    document.getElementById('image-container').innerHTML = '';
+    var imageContainer = document.getElementById('image-container');
+    var picture = new Image();
+
+    // Set the source for the sliding picture
+    picture.src = 'received_2030241160457690.jpeg'; // Update to your image file
+    picture.alt = 'Sliding Picture';
+    picture.className = 'sliding-picture'; // Add class for slide animation
+
+    picture.onload = function () {
+        imageContainer.appendChild(picture);
+        // After slide-in animation, show the cat-heart picture
+        setTimeout(displayCatHeart, 3000); // Wait 3 seconds before transitioning
+    };
+}
+
+// Function to display the cat-heart picture
+function displayCatHeart() {
+    document.getElementById('image-container').innerHTML = '';
+    var imageContainer = document.getElementById('image-container');
+    var catHeartImage = new Image();
+
+    catHeartImage.src = '1738932995490photoSticker.jpg'; // Update to your cat-heart image
+    catHeartImage.alt = 'Cat Heart';
+    catHeartImage.onload = function () {
+        imageContainer.appendChild(catHeartImage);
+        document.getElementById('options').style.display = 'none'; // Hide buttons
+    };
+}
+
 function displayCat() {
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
@@ -56,25 +82,4 @@ function displayCat() {
     };
 }
 
-// Function to display the cat-heart.gif
-function displayCatHeart() {
-    // Clear existing content in the image container
-    document.getElementById('image-container').innerHTML = '';
-    // Get the container where the image will be displayed
-    var imageContainer = document.getElementById('image-container');
-    // Create a new Image element for the cat-heart
-    var catHeartImage = new Image();
-    // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
-    // Set alternative text for the image (for accessibility)
-    catHeartImage.alt = 'Cat Heart';
-    // When the cat-heart image is fully loaded, add it to the image container
-    catHeartImage.onload = function() {
-        imageContainer.appendChild(catHeartImage);
-        // Hide the options container
-        document.getElementById('options').style.display = 'none';
-    };
-}
-
-// Display the cat.gif initially
 displayCat();
